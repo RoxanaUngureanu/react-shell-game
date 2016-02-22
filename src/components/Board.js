ea8ded3202588d8864a21765b829be39fc89a704
ROXANA UNGUREANU <ungureanu.roxi@yahoo.com>
2016-02-22 04:43:57 +0200

    made win functionality -- need to fix some bugs

--- src/components/Board.js
+++ src/components/Board.js
@@ -8,13 +8,51 @@ var Board = React.createClass({
   getInitialState: function() {
     var board = [];
     for (var i=0; i<3; i++) {
-      board.push({index: i+1});
+      board.push({number: i});
     }
     return {
-      board: board
+      board: board,
+      showTheBall:false,
+      clickedCup : 0,
+      gameStatus:''
     }
   },
-  RestartGame: function(){
+  render: function(){
+    var cups = [];
+    for (var i=0; i<3; i++) {
+
+      var cup = <Cup index={i} key={i} showTheBall={ i === this.state.showTheBall}
+                     winner = {i === this.props.index}
+                     onClick={this.onCupClick}/>;
+
+      cups.push(cup);
+    }
+    return (
+      <div>
+        <h1 className = "header">Shell Game</h1>
+        <button className = "button" onClick={this.shuffleCups}>Shuffle!</button>
+        <button className = "button" onClick={this.shuffleCups}>Restart!</button>
+        <div className="cup-container">
+          {cups}
+          <h1 className="game-status"> {this.state.gameStatus} </h1>
+        </div>
+      </div>
+    )
+  },
+
+  onCupClick: function(index){
+    this.setState({clickedCup:index, showTheBall:index});
+    var array = this.state.board;
+    for (var i = 0; i < array.length; i++){
+      if(index === this.state.board[i].number) {
+        this.setState({gameStatus: 'You Won!'});
+      } else {
+        this.setState({gameStatus: 'You lost :('});
+      }
+    }
+  },
+
+  shuffleCups: function(){
     var boardArray = this.state.board;
     var index = boardArray.length;
     var obj, randomIndex;
@@ -25,27 +63,24 @@ var Board = React.createClass({
       boardArray[index] = boardArray[randomIndex];
       boardArray[randomIndex] = obj;
     }
-    this.setState({boardArray});
-    console.log(boardArray)
-  },
-  render: function(){
-    var cups = [];
-    for (var i=0; i<3; i++) {
-      var cup = <Cup
-        index={i}
-        key={i}
-      />;
-      cups.push(cup);
-    }
-    return (
-      <div>
-        <h1 className = "header">Shell Game</h1>
-        <button className = "button" onClick={this.RestartGame}>Restart the game!</button>
-        <div className="cup-container">
-          {cups}
-        </div>
-      </div>
-    )
+    this.setState({board:boardArray});
   }
+  //isWinner: function(index){
+  //  var array = this.state.board;
+  //  for (var i = 0; i < array.length; i++){
+  //    console.log("Array number ", array[i].number);
+  //    console.log("index ", index);
+  //    if (array[i].number == index){
+  //      console.log("WINNER!!!!!!!!!!!!!!!!!!!!!")
+  //      console.log('CONSOLE FIRST ' + array[i].number + ' ' + index)
+  //
+  //      return false }
+  //    //} else {
+  //    //  console.log("NOT A WINNER!")
+  //    //  console.log('CONSOLE SECOND ' + array[i].number + ' ' + index)
+  //    //}
+  //  }
+  //  return false
+  //}
 });
 export default Board;
--- src/components/Cup.js
+++ src/components/Cup.js
@@ -4,9 +4,27 @@ require('styles/App.css');
 import React from 'react';
 
 var Cup = React.createClass({
+  onClick: function () {
+    this.props.onClick(this.props.index);
+  },
   render:function(){
+    var cupImage = 'url("http://www.clker.com/cliparts/c/v/W/h/v/Z/silver-cup-th.png")no-repeat';
+    var ballImage = 'url("http://d.alternativeto.net/dist/icons/my-crystal-ball_71748.png?width=64&height=64&mode=crop&upscale=false")no-repeat';
+    var cupStyle = {background: '#fffdea', backgroundSize:'100%'};
+    if (this.props.showTheBall === false) {
+      cupStyle.background = cupImage;
+      cupStyle.backgroundSize = '100%';
+      console.log('ONE')
+    } else if ((this.props.showTheBall === true)&&(this.props.winner === true)){
+      cupStyle.background = ballImage;
+      console.log('TWO')
+    } else if ((this.props.showTheBall === true)&&(this.props.winner === false)) {
+      cupStyle.background = '#fffdea';
+      console.log('THREE')
+    }
     return (
-      <img className='cup' src="/images/silver-cup-th.png"/>
+      <div className='cup'  style={cupStyle} onClick={this.onClick}>
+      </div>
     )
   }
 });
